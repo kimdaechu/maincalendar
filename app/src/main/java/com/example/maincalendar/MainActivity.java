c2eededb1210d679b4f3bf5a3b38e4f808ff33f6
kimdaehyun <redfox6514@gmail.com>
2023-05-18 20:51:15 +0900

    MAINCALENDAR

--- app/src/main/java/com/example/maincalendar/MainActivity.java
+++ app/src/main/java/com/example/maincalendar/MainActivity.java
@@ -2,18 +2,24 @@ package com.example.maincalendar;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.style.ForegroundColorSpan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
 import com.prolificinteractive.materialcalendarview.DayViewFacade;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     private MaterialCalendarView calendarView;
+    private Set<CalendarDay> selectedDates;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,28 +27,74 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         calendarView = findViewById(R.id.calendarView);
+        selectedDates = new HashSet<>();
+
         calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
             @Override
             public void onDateSelected(MaterialCalendarView widget, CalendarDay date, boolean selected) {
                 checkDateAvailability(date.getDate());
             }
         });
+
+        // 토요일과 일요일 표시
+        calendarView.addDecorator(new SaturdayDecorator());
+        calendarView.addDecorator(new SundayDecorator());
+        calendarView.addDecorator(new EventDecorator());
     }
 
     private void checkDateAvailability(Date date) {
-        // 서버에서 해당 날짜의 가능 여부를 가져온 후 UI에 반영하는 코드를 작성합니다.
-        // 이 부분은 서버와 통신하는 방법에 따라 다를 수 있습니다.
-        // 서버에서 가져온 가능 여부를 isAvailable 변수에 저장합니다.
-
-        boolean isAvailable = true; // 예시로 true로 설정했습니다.
-
         CalendarDay day = CalendarDay.from(date);
-        if (isAvailable) {
-            calendarView.setDateSelected(day, true);
-            calendarView.setSelectionColor(Color.GREEN);
+        if (selectedDates.contains(day)) {
+            selectedDates.remove(day);
         } else {
-            calendarView.setDateSelected(day, false);
-            calendarView.setSelectionColor(Color.TRANSPARENT);
+            selectedDates.add(day);
         }
+        calendarView.invalidateDecorators(); // 데코레이터 갱신
+    }
+
+    private class SaturdayDecorator implements DayViewDecorator {
+        @Override
+        public boolean shouldDecorate(CalendarDay day) {
+            Calendar calendar = day.getCalendar();
+            int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+            return dayOfWeek == Calendar.SATURDAY;
+        }
+
+        @Override
+        public void decorate(DayViewFacade view) {
+            view.addSpan(new ForegroundColorSpan(Color.BLUE)); // 날짜 숫자 색상 변경
+        }
+    }
+
+    private class SundayDecorator implements DayViewDecorator {
+        @Override
+        public boolean shouldDecorate(CalendarDay day) {
+            Calendar calendar = day.getCalendar();
+            int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+            return dayOfWeek == Calendar.SUNDAY;
+        }
+
+        @Override
+        public void decorate(DayViewFacade view) {
+            view.addSpan(new ForegroundColorSpan(Color.RED)); // 날짜 숫자 색상 변경
+        }
+    }
+
+    private class EventDecorator implements DayViewDecorator {
+        @Override
+        public boolean shouldDecorate(CalendarDay day) {
+            return selectedDates.contains(day);
+        }
+
+        @Override
+        public void decorate(DayViewFacade view) {
+            view.setSelectionDrawable(getResources().getDrawable(R.drawable.green_circle));
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        calendarView.invalidateDecorators(); // 데코레이터 갱신
     }
 }
