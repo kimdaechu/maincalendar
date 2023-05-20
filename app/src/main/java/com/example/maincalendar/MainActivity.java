c1e60ded4dc9fdf64b08c287c3f4aaab71edcc27
qudwn <qudwns365@gmail.com>
2023-05-20 23:28:24 +0900

    카카오 연동 예)

--- app/src/main/java/com/example/maincalendar/MainActivity.java
+++ app/src/main/java/com/example/maincalendar/MainActivity.java
@@ -1,17 +1,26 @@
 package com.example.maincalendar;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.style.ForegroundColorSpan;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.DayViewDecorator;
 import com.prolificinteractive.materialcalendarview.DayViewFacade;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import android.util.Base64;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashSet;
@@ -21,10 +30,37 @@ public class MainActivity extends AppCompatActivity {
     private MaterialCalendarView calendarView;
     private Set<CalendarDay> selectedDates;
 
+    //파이어베이스 실시간 데이터베이스 선언
+    private DatabaseReference mDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //카카오톡 해시키를 얻는 메소드
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageInfo == null)
+            Log.e("KeyHash", "KeyHash:null");
+
+        for (Signature signature : packageInfo.signatures) {
+            try {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            } catch (NoSuchAlgorithmException e) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+            }
+        }
+
+        //파이어베이스 실시간 데이터베이스
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         calendarView = findViewById(R.id.calendarView);
         selectedDates = new HashSet<>();
@@ -64,6 +100,7 @@ public class MainActivity extends AppCompatActivity {
         public void decorate(DayViewFacade view) {
             view.addSpan(new ForegroundColorSpan(Color.BLUE)); // 날짜 숫자 색상 변경
         }
+
     }
 
     private class SundayDecorator implements DayViewDecorator {
@@ -98,3 +135,5 @@ public class MainActivity extends AppCompatActivity {
         calendarView.invalidateDecorators(); // 데코레이터 갱신
     }
 }
+// Git 연동 테스트용 주석
+//아이고난1 아이고난2 흐헤헤
---
+++ app/src/main/java/com/example/maincalendar/User.java
@@ -0,0 +1,26 @@
+package com.example.maincalendar;
+
+public class User {
+    private String userName;
+    private String date;
+
+    public User(){
+
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
